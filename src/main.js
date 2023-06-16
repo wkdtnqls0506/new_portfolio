84998dce5c6fe5552c8adb80272155331a277fe1
Subin Jang <wkdtnqls0506@naver.com>
2023-06-16 11:58:25 +0900

    JS-Home

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
-// Header에 페이지 아래로 스크롤시 dark 스타일링 적용
+// Header가 페이지 아래로 스크롤시 dark 스타일링 적용
 const header = document.querySelector('.header');
-const headerHeight = header.getBoundingClientRect().height;
+const headerHeight = header.offsetHeight;
 document.addEventListener('scroll', () => {
-    if(window.scrollY >headerHeight) {
+    if(window.scrollY > headerHeight) {
         header.classList.add('header--dark');
     } else {
         header.classList.remove('header--dark');
     }
+});
+
+// 홈 섹션을 아래로 스크롤시 투명하게 처리함
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
 });
