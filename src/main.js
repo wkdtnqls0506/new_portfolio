e143df7b6f9699ec46adff91c64549cb36071374
Subin Jang <wkdtnqls0506@naver.com>
2023-06-16 12:18:08 +0900

    JS-Arrow up

--- src/main.js
+++ src/main.js
@@ -13,5 +13,15 @@ document.addEventListener('scroll', () => {
 const home = document.querySelector('.home__container');
 const homeHeight = home.offsetHeight;
 document.addEventListener('scroll', () => {
-    home.style.opacity = 1 - window.scrollY / homeHeight;
+    home.style.opacity = 1 - window.scrollY / homeHeight;  // 세세하게 나타내야하기 때문에 js에서 설정
+});
+
+// 화살표 아이콘 숨기기
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+    if (window.scrollY > homeHeight / 2) {
+        arrowUp.style.opacity = 1;
+    } else {
+        arrowUp.style.opacity = 0;
+    }
 });
